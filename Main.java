23841dc091ae2382ca93bbe26cdc6b7166756f22
Limbo2Forma <46713895+Limbo2Forma@users.noreply.github.com>
2019-01-16 18:18:47 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,50 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.util.Properties;
 
-public class Main extends Application {
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-    @Override
-    public void start(Stage primaryStage){
-        primaryStage.setTitle("Assignment 3");
-        Controller controller = new Controller();
-        primaryStage.setScene(new Scene(controller.calendarView.getView()));
-        primaryStage.setResizable(false);
-        primaryStage.show();
+public class Main {
+    private static Session sessionMail(){
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("namvu6799@gmail.com", "nam671999");
+                    }
+                });
+        return  session;
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    private static void sendEmail(String receiver,String subject, String content){
+        try {
+
+            Message message = new MimeMessage(sessionMail());
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(receiver));
+            message.setSubject(subject);
+            message.setText(content);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static  void main(String[] args) {
+        sendEmail("nam671999hai@gmail.com","Test mail 2","Test mail sent again");
+        sendEmail("s3695302@rmit.edu.vn","Test mail 3","Test mail sent again");
     }
 }
