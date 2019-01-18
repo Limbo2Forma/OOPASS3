38f082053b8991c3ad224b349db832239cc4617c
Limbo2Forma <46713895+Limbo2Forma@users.noreply.github.com>
2019-01-19 01:58:49 +0700

    Add files via upload

---
+++ LiveClockTest/src/Main.java
@@ -0,0 +1,44 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import sample.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Add Event pop up");
+        Button btn = new Button();
+        btn.setText("Add Event");
+        btn.setOnAction(e ->
+                showAddEvent()
+        );
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+    public void showAddEvent(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/sample/event.fxml"));
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Add Event");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception ioe){
+            System.out.println("ioe error");
+        }
+    }
+}
