b5389cb4e02f91f32a2bc5f28da6e18dad1b88d3
Limbo2Forma <46713895+Limbo2Forma@users.noreply.github.com>
2019-01-21 21:56:54 +0700

    Update DataLoad.java

--- LiveClockTest/src/Controllers/DataLoad.java
+++ LiveClockTest/src/Controllers/DataLoad.java
@@ -1,43 +1,78 @@
 package Controllers;
 
 import Models.Event;
+import Models.User;
 
 import java.io.*;
 import java.util.ArrayList;
 
 public class DataLoad {
     public static ArrayList<Event> eventList = new ArrayList<>();
+    public static User user;
+
+    public static void loadAlldata(){
+        deserializeEvent();
+        deserializeUser();
+    }
+
+    public static void saveAllData(){
+        serializeEvent();
+        serializeUser();
+    }
+
+    public static void serializeUser(){
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream("UserSaveData.txt");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(user);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+    public static void deserializeUser(){
+        try {
+            FileInputStream fileIn = new FileInputStream("UserSaveData.txt");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            user = (User) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("User class not found");
+            c.printStackTrace();
+        }
+    }
 
     public static void serializeEvent(){
-        try
-        {
+        try {
             FileOutputStream fos = new FileOutputStream("EventSaveData.txt");
             ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("EventSaveData.txt"));
             oos.writeObject(eventList);
             oos.close();
             fos.close();
         }
-        catch (IOException ioe)
-        {
+        catch (IOException ioe) {
             ioe.printStackTrace();
         }
     }
+
     public static void deserializeEvent(){
-        try
-        {
+        try {
             FileInputStream fis = new FileInputStream("EventSaveData.txt");
             ObjectInputStream ois = new ObjectInputStream(fis);
             eventList = (ArrayList) ois.readObject();
             ois.close();
             fis.close();
         }
-        catch (IOException ioe)
-        {
+        catch (IOException ioe) {
             ioe.printStackTrace();
             return;
         }
-        catch (ClassNotFoundException c)
-        {
+        catch (ClassNotFoundException c) {
             System.out.println("Class not found");
             c.printStackTrace();
             return;
